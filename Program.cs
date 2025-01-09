da7bc6da6e96eba43ce408effa308b4ec4194a65
jeanettee8 <ekje112024@gmail.com>
2025-01-09 12:21:44 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double temp=0;
+
+if (temp<1) {
+    Console.WriteLine("Be careful, it might be slippery outside");
+}
+
+else if (temp<15) {
+    Console.WriteLine("Temperature outside is normal");
+}
+
+else if (temp<20) {
+    Console.WriteLine("You should start wearing sunscreen");
+}
+
+else if (temp>=20) {
+    Console.WriteLine("It's very hot outside");
+}
